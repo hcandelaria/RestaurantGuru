609cb8113cc45dab0f32cb8d4c6b0b1f6b96ebf1
unknown <hcandelaria66.7@gmail.com>
2019-04-11 22:32:18 -0400

    Updated LaunchScreen

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,32 +1,32 @@
 import React, { Component } from 'react'
 import { ScrollView, Text, Image, View } from 'react-native'
 import DevscreensButton from '../../ignite/DevScreens/DevscreensButton.js'
+import { connect } from 'react-redux'
 
-import { Images } from '../Themes'
 
 // Styles
 import styles from './Styles/LaunchScreenStyles'
+import BottomNavBar from '../Components/BottomNavBar/';
 
-export default class LaunchScreen extends Component {
+import CategoriesView from './CategoriesView/';
+
+class LaunchScreen extends Component {
+  constructor(props, context){
+    super(props,context);
+  }
   render () {
     return (
       <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
-        <ScrollView style={styles.container}>
-          <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
-          </View>
-
-          <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
-            </Text>
-          </View>
-
-          <DevscreensButton />
-        </ScrollView>
+        <CategoriesView navigation={this.props.navigation} />
+        <BottomNavBar />
       </View>
     )
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    categories: state.categories.categories,
+    restaurants: state.restaurants.restaurants,
+  }
+}
+export default connect(mapStateToProps, null)(LaunchScreen)
