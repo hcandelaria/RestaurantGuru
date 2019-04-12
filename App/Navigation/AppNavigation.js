f690d67c1a9dc33f509dd12d0bdcf776acc90eea
unknown <hcandelaria66.7@gmail.com>
2019-04-11 22:33:11 -0400

    Updated Navigation

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,17 +1,23 @@
+import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 import LaunchScreen from '../Containers/LaunchScreen'
-
+import RestaurantsView from '../Containers/RestaurantsView'
+import CategoriesView from '../Containers/CategoriesView/';
+import RestaurantDetailsView from '../Containers/RestaurantDetailsView/';
+import HeaderNavBar from '../Containers/HeaderNavBar'
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+  LaunchScreen: { screen: LaunchScreen },
+  CategoriesView: { screen: CategoriesView },
+  RestaurantsView: { screen: RestaurantsView },
+  RestaurantDetailsView: { screen: RestaurantDetailsView },
 }, {
-  // Default config for all screens
-  headerMode: 'none',
   initialRouteName: 'LaunchScreen',
-  navigationOptions: {
-    headerStyle: styles.header
+  // Default config for all screens
+  defaultNavigationOptions: {
+    header: ({navigation}) => <HeaderNavBar style={styles} navigation={navigation} />
   }
 })
 
--- App/Navigation/Styles/NavigationStyles.js
+++ App/Navigation/Styles/NavigationStyles.js
@@ -1,8 +1,12 @@
 import { StyleSheet } from 'react-native'
 import { Colors } from '../../Themes/'
+import { Platform } from 'react-native';
 
 export default StyleSheet.create({
   header: {
-    backgroundColor: Colors.backgroundColor
+    backgroundColor: Colors.backgroundColor,
+    flexDirection: "row",
+    height: 80,
+    marginTop: Platform.OS == "ios" ? 20 : 0 //only for IOS to give StatusBar Space
   }
 })
